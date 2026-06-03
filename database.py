e9e2820c2abe9594c22341e25e23185ecea924b5
abhishek-pandey7 <abhshkp@gmail.com>
2026-06-03 22:03:37 +0530

    Migrate from SQLite to PostgreSQL

--- database.py
+++ database.py
@@ -1,16 +1,20 @@
-import sqlite3
+import psycopg2
 
 class URLDatabase:
-    def __init__(self,db_path='urls.db'):
-        self.db_path=db_path
+    def __init__(self,db_url='postgresql://postgres:password@localhost:5432/url-shortener'):
+        self.db_url=db_url
         self._init_db()
     
+    def _get_connection(self):
+        return psycopg2.connect(self.db_url)
+        #Postgres creates a fresh connection per request
+
     def _init_db(self):
         #creating table where id is primary key
-        with sqlite3.connect(self.db_path) as conn:
+        with self._get_connection() as conn:
             conn.execute("""
             CREATE TABLE IF NOT EXISTS urls(
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                id SERIAL PRIMARY KEY,
                 long_url TEXT NOT NULL,
                 clicks INTEGER DEFAULT 0,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -22,18 +26,19 @@ class URLDatabase:
         """Inserts a new long URL into the database
         Return's the auto-generated ID
         """
-        with sqlite3.connect(self.db_path) as conn:
+        with self._get_connection() as conn:
             cursor=conn.cursor()
 
-            cursor.execute("INSERT INTO urls (long_url) VALUES (?)",(long_url,))
-            return cursor.lastrowid
+            cursor.execute("INSERT INTO urls (long_url) VALUES (%s) RETURNING id",(long_url,))
+            new_id=cursor.fetchone()[0]
+            conn.commit()
+            return new_id
     
     def get_by_id(self,db_id:int) -> str | None:
         """Retrieves long url using the database row ID """
-        with sqlite3.connect(self.db_path) as conn:
-            cursor=conn.cursor()
-
-            cursor.execute("SELECT long_url FROM urls WHERE id=? ", (db_id,))
-            row=cursor.fetchone()
-            return row[0] if row else None
+        with self._get_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT long_url FROM urls WHERE id=%s ", (db_id,))
+                row=cursor.fetchone()
+                return row[0] if row else None
     
