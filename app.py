7558f57a0e7a61cd80a835a46922540de49434ad
abhishek-pandey7 <abhshkp@gmail.com>
2026-06-09 23:03:16 +0530

    feat: add url validation, rate limiting, and graceful degradation

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import ipaddress
 from flask import Flask,request,redirect,jsonify
 from database import URLDatabase
 import base62
 import redis
+from urllib.parse import urlparse
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app=Flask(__name__)
 
@@ -18,7 +22,38 @@ redis_client=redis.Redis(
     #raw b-strings (b'http...').
 )
 
+#initializing our rate limiter using Redis as storage
+limiter=Limiter(
+    get_remote_address,
+    app=app,
+    storage_uri='redis://localhost:6379/1',#using db 1 for rate limiting data,leaving db 0 for our url cache
+    strategy='fixed-window'
+)
+
+def is_valid_url(url):
+    try:
+        parsed=urlparse(url)
+        if parsed.scheme not in ('http','https'):
+            return False
+        
+        #prevent SSRF (server side request forgery
+        #occurs when an application fetches a user-supplied URL without properly validating or sanitizing it.
+        #block local/private IPs (like 127.0.0.1 or 169.254.169.254)
+        if parsed.hostname:
+            try:
+                ip=ipaddress.ip_address(parsed.hostname)
+                #if ip is local or private raise ValueError
+                if ip.is_loopback or ip.is_private:
+                    return False
+            except ValueError:
+                pass
+        return True
+    except Exception as e:
+        print(f"URL Validation Error: {e}")
+        return False
+
 @app.route('/shorten',methods=['POST'])
+@limiter.limit('10 per minute') #limit url creation to prevent storage exhaustion
 def shorten():
     data=request.get_json()
     long_url=data.get('long_url')
@@ -26,6 +61,9 @@ def shorten():
     if not long_url:
         return jsonify({'error':'missing url'}),400
     
+    if not is_valid_url(long_url):
+        return jsonify({'error':'Invalid or Unrestricted URL'}),400
+
     #database write: get autoincrementing counter id
     db_id=db_repo.insert(long_url)
 
@@ -43,15 +81,18 @@ def shorten():
 def redirect_url(short_id):
     #Cache-Aside pattern with Redis
     #1. check redis first
-    cached_url=redis_client.get(short_id)
+    try:
+        cached_url=redis_client.get(short_id)
 
-    if(cached_url):
-        print(f"Cache hit! Redirecting immediately.")
-        redis_client.incr(f"clicks:{short_id}")
-        return redirect(cached_url)
+        if(cached_url):
+            print(f"Cache hit! Redirecting immediately.")
+            redis_client.incr(f"clicks:{short_id}")
+            return redirect(cached_url)
 
-    print(f"Cache miss for {short_id}. Hitting SQLite...")    
-
+    except redis.RedisError as e:
+        print(f"Redis is down or unreachable: {e}. Falling back to Database.")
+    
+    print(f"Cache miss (or Redis down) for {short_id}. Hitting DB...")
     #2. cache miss : decode string to integer ID
     try:
         db_id=base62.decode(short_id)
@@ -65,10 +106,14 @@ def redirect_url(short_id):
         return jsonify({'error':'URL not found'}),404
     
     #4. populate redis cache for next user
-    redis_client.set(short_id,long_url,ex=86400)
-    #ex=86400 sets a TTL (Time to Live) of 24 hrs. This automatically handles URL Cache Expiry
-    #We also increment a counter for stats.
-    redis_client.incr(f"clicks:{short_id}")
+    try:
+        redis_client.set(short_id,long_url,ex=86400)
+        #ex=86400 sets a TTL (Time to Live) of 24 hrs. This automatically handles URL Cache Expiry
+        #We also increment a counter for stats.
+        redis_client.incr(f"clicks:{short_id}")
+    
+    except redis.RedisError as e:
+        print(f"Failed to write to cache: {e}")
 
     #redirect
     return redirect(long_url)
